9a5ef86a39e6051ca9a1e3477ebed90ef8587867
Yogeshsorde <dineshsorde1997@gmail.com>
2022-12-07 11:42:07 +0530

    please check prime problem

---
+++ prime.js
@@ -0,0 +1,20 @@
+let num=13
+let count=0
+
+for(let i=2;i<=num;i++){
+
+     if(num%2==0){
+
+       count++
+    }
+
+    }
+if(count==1){
+
+   console.log(i,"is prime number")
+
+}else
+   
+   {
+
+   console.log(i ,"is Not a prime number")
